252852ed0e42d4fb0d5f1d0b02840bae5a39ce67
Ftemur <153289154+TMFZ223@users.noreply.github.com>
2024-10-19 21:23:41 +0300

    Update TestAuthorization.java

--- src/test/java/TestAuthorization.java
+++ src/test/java/TestAuthorization.java
@@ -1,20 +1,33 @@
+import org.junit.jupiter.params.provider.Arguments;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.List;
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestAuthorization {
     private WebDriver driver;
     private String MainPageUrl = "https://www.saucedemo.com/";
+    static List<String> names = Arrays.asList("standard_user", "locked_out_user", "problem_user", "performance_glitch_user", "error_user", "visual_user", "standard_using", "Arseniy", "select", "<script>", "");
+    static List<String> passwords = Arrays.asList("secret_sauce", "secret_sauc", "select", "</script>", "");
+
+    static Stream<Arguments> provideTestArguments() {
+        List<Arguments> argumentsList = new ArrayList<>();
+        for (String name : names) {
+            for (String password : passwords) {
+                argumentsList.add(Arguments.of(name, password));
+            }
+        }
+        return argumentsList.stream();
+    }
 
     @BeforeEach
     public void setUp() {
@@ -30,30 +43,15 @@ public class TestAuthorization {
     }
 
     @ParameterizedTest
-    @CsvSource({
-            "standard_user, secret_sauce",
-            "locked_out_user, secret_sauce",
-            "problem_user, secret_sauce",
-            "performance_glitch_user, secret_sauce",
-            "error_user, secret_sauce",
-            "visual_user, secret_sauce",
-            ", secret_sauce",
-            "Arseniy, secret_sauce",
-            " , secret_sauce",
-            "<script>, secret_sauce",
-            "select, secret_sauce",
-            "standard_user, secret_s",
-            "standard_using, secret_sa",
-            ","
-    })
-
+    @MethodSource("provideTestArguments")
     public void testCheckAuthorization(String name, String password) throws InterruptedException {
         AuthorizationPage authorizationPage = new AuthorizationPage(driver);
         authorizationPage.enterUsername(name);
         authorizationPage.enterPassword(password);
         authorizationPage.clickLoginButton();
         String expectedUrl;
-        if ("standard_user".equals(name) && "secret_sauce".equals(password) || "locked_out_user".equals(name) && "secret_sauce".equals(password) || "problem_user".equals(name) && "secret_sauce".equals(password) || "performance_glitch_user".equals(name) && "secret_sauce".equals(password) || "error_user".equals(name) && "secret_sauce".equals(password) || "visual_user".equals(name) && "secret_sauce".equals(password)) {
+        List<String> AcceptedUsers = Arrays.asList("standard_user", "locked_out_user", "problem_user", "performance_glitch_user", "error_user", "visual_user");
+        if (AcceptedUsers.contains(name) && "secret_sauce".equals(password)) {
             expectedUrl = "https://www.saucedemo.com/inventory.html";
         } else {
             expectedUrl = "https://www.saucedemo.com/";
